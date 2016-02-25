247394aa110c589ff92b7aa19b5b8f5593f0c303
benjaminpettus <benjaminpettus@gmail.com>
2016-02-24 21:20:15 -1000

    value

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp');
 
 
-myApp.controller('MyController', ['$scope', function ($scope) {
+myApp.controller('MyController', [
+  '$scope', 
+  'mainCharacter', 
+  function ($scope, mainCharacter) {
   $scope.myFirstName = "Ben";
   $scope.myModel = "Ready Player One!";
+  $scope.mainCharacter = mainCharacter;
 }]);
