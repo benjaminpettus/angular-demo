f544b3693d4fb1fbe9952380aaf8a92de16f83b5
benjaminpettus <benjaminpettus@gmail.com>
2016-02-24 20:48:53 -1000

    module created

---
+++ public/js/myApp.js
@@ -0,0 +1,11 @@
+angular.module('myApp', []);
+
+var myApp = angular.module('myApp');
+
+myApp
+  .config(function () {
+
+  })
+  .run(function () {
+    console.log('start');
+  });console.log("WTF");
