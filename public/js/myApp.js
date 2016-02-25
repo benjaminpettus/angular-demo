38d077b7a9958bb7fc0fd8e47a1bcc2c82aa46b3
benjaminpettus <benjaminpettus@gmail.com>
2016-02-24 21:39:11 -1000

    constant

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -6,9 +6,13 @@ var myApp = angular.module('myApp');
 
 myApp
   .config(function () {
-
+    //set up configuration
   })
-  .run(function () {
-    console.log('start');
-  });
+  .run([
+    '$rootScope',
+    'APP_VERSION',
+    function ($rootScope, APP_VERSION) {
+    //start application 
+    $rootScope.version = APP_VERSION;
+  }]);
 
