f90c5eb0af0ed584bfba2f64f07250769154c877
benjaminpettus <benjaminpettus@gmail.com>
2016-02-25 22:30:31 -1000

    working, fixed routes

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -11,6 +11,18 @@ myApp
 
     //routes
     $routeProvider
+      .when('/', {
+        templateUrl: 'templates/intro.html',
+        controller: 'MyController'
+      })
+      .when('/books', {
+        templateUrl:'templates/books.html',
+        controller: 'MyController'
+      })
+      .when('/movies', {
+        templateUrl: 'templates/movies.html',
+        controller: 'MyController'
+      })
       .when('/404', {
         templateUrl: '/templates/404.html'
       })
